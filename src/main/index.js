cca054aa84d40f61ce753e3a1e892361e70b151d
Takenokos <595693858@qq.com>
2018-12-11 17:08:14 +0800

    push

---
+++ src/main/index.js
@@ -0,0 +1,102 @@
+'use strict'
+
+import { app, BrowserWindow, ipcMain, Tray, Menu } from 'electron'
+import path from 'path'
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    frame: false // 去除菜单
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+  // 自定义菜单按钮
+  // 关闭按钮
+  ipcMain.on('close-window', () => {
+    mainWindow.close()
+  })
+  // 最小化
+  ipcMain.on('min-window', () => {
+    mainWindow.minimize()
+  })
+  // 禁止直接关闭
+  mainWindow.on('close', (event) => {
+    mainWindow.hide()
+    mainWindow.setSkipTaskbar(true)
+    event.preventDefault()
+  })
+  mainWindow.on('show', () => {
+    tray.setHighlightMode('always')
+  })
+  mainWindow.on('hide', () => {
+    tray.setHighlightMode('never')
+  })
+  // 系统托盘
+  const tray = new Tray(path.join(__dirname, '../../build/icons/icon.ico'))
+  const contextMenu = Menu.buildFromTemplate([
+    { label: '退出', click: () => { mainWindow.destroy() } } // 我们需要在这里有一个真正的退出（这里直接强制退出）
+  ])
+  tray.setToolTip('My托盘测试')
+  tray.setContextMenu(contextMenu)
+  tray.on('click', () => { // 我们这里模拟桌面程序点击通知区图标实现打开关闭应用的功能
+    if (!mainWindow.isVisible()) {
+      mainWindow.show()
+    }
+    mainWindow.isVisible() ? mainWindow.setSkipTaskbar(false) : mainWindow.setSkipTaskbar(true)
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/views/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+    <div class="menu">
+      <a
+        class="min"
+        @click="min"
+      >-</a>
+    </div>
+    hello word
+  </div>
+</template>
+<script>
+// const { ipcRenderer } = require('electron')
+import { ipcRenderer } from 'electron'
+export default {
+  name: 'index',
+  methods: {
+    min () {
+      ipcRenderer.send('min-window')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.main {
+  position: relative;
+  .menu {
+    .min {
+      cursor: pointer;
+    }
+  }
+}
+</style>
